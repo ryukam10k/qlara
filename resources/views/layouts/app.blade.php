1af5ccb93f4955cdae6845802ac51b13679fec30
ryukam10k <r.kamioka@gmail.com>
2019-03-09 15:40:05 +0900

    Add Left NavItems

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="/deal">Deals</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/customer">Customers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/dealcategory">DealCategories</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->

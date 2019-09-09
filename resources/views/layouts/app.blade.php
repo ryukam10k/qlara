be2c7aca07d31b21b7681b42567d5197ad25a824
ryukam10k <r.kamioka@gmail.com>
2019-09-09 22:31:39 +0900

    ユーザー登録は非表示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,11 +50,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
                         @else
                             @if (Auth::user()->role->is_admin == true)
                                 <li class="nav-item dropdown">
